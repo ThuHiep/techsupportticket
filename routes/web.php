499f17ad5863cd31123581e726f947f19bdd8872
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2024-12-03 19:24:19 +0700

    init
    test login, thêm các template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Backend\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', [AuthController::class, 'login'])->name('auth.login');
