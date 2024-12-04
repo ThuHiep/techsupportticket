e57fb6474a72f9cba125c245d59a3f2e2312a4b5
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2024-12-04 14:41:08 +0700

    int
    icon giao diện

--- resources/views/backend/dashboard/component/sidebar.blade.php
+++ resources/views/backend/dashboard/component/sidebar.blade.php
@@ -23,14 +23,14 @@
             <li class="active">
                 <a href="{{ route('dashboard.index') }}"><i class="fa fa-th-large"></i> <span class="nav-label">Trang Quản Trị</span> <span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
-                    <li><a href="{{ route('user.index') }}">Tài khoản</a></li>
-                    <li><a href="index.html">Phân quyền</a></li>
-                    <li><a href="index.html">Nhân viên</a></li>
-                    <li><a href="index.html">Khách hàng</a></li>
-                    <li><a href="index.html">Phòng ban</a></li>
-                    <li><a href="index.html">Yêu cầu</a></li>
-                    <li><a href="index.html">Thống kê</a></li>
-                    <li><a href="index.html">Tin tức</a></li>
+                    <li><a href="{{ route('user.index') }}">  <i class="fas fa-id-card"></i>Tài khoản</a></li>
+                    <li><a href="index.html"><i class="fas fa-shield-alt"></i>Phân quyền</a></li>
+                    <li><a href="index.html"> <i class="fas fa-address-book"></i>Nhân viên</a></li>
+                    <li><a href="{{ route('backend.customer.hienthi') }}"> <i class="fa-solid fa-users"></i>Khách hàng</a></li>
+                    <li><a href="index.html"> <i class="fa-solid fa-clipboard"></i>Phòng ban</a></li>
+                    <li><a href="index.html"> <i class="fa-solid fa-tools"></i>Yêu cầu</a></li>
+                    <li><a href="index.html"> <i class="fa-solid fa-chart-line"></i>Thống kê</a></li>
+                    <li><a href="index.html"> <i class="fa-solid fa-newspaper"></i>Tin tức</a></li>
                 </ul>
             </li>
         </ul>
--- resources/views/backend/dashboard/layout.blade.php
+++ resources/views/backend/dashboard/layout.blade.php
@@ -2,6 +2,8 @@
 <html>
 
 <head>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 
     @include('backend.dashboard.component.header')
 
