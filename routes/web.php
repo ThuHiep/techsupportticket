27a75961b57d0c4c05fab0306e1a8a4e734ddadb
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2024-12-13 00:26:36 +0700

    INIT
    faq edit

--- routes/web.php
+++ routes/web.php
@@ -121,6 +121,7 @@ Route::name('faq.')->group(function () {
     Route::get('/faq/create', [FaqController::class, 'create'])->name('create');
     Route::post('/faq/store', [FaqController::class, 'store'])->name('store');
     Route::get('/faq/edit/{faq_id}', [FaqController::class, 'edit'])->name('edit');
+    Route::put('/faq/update/{faq_id}', [FaqController::class, 'update'])->name('update');
     Route::delete('/faq/delete/{faq_id}', [FaqController::class, 'destroy'])->name('delete');
 
 });
