a339853f9d5abff01634a0f85ffb39f56dd8cd32
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2024-12-29 19:57:53 +0700

    responsive thongke

--- resources/views/admin/statistical/index.blade.php
+++ resources/views/admin/statistical/index.blade.php
@@ -27,13 +27,7 @@
             color: orange;
             text-align: left;
         }
-        .report-section {
-            background: #fff;
-            border-radius: 8px;
-            padding: 10px;
-            margin: 10px 0;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-        }
+
 
         .progress-bar {
             width: 100%;
