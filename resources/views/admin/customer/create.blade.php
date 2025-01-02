30ff5775b211e63f0f838e179be91bb302751476
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2025-01-02 21:43:41 +0700

    Update create.blade.php

--- resources/views/admin/customer/create.blade.php
+++ resources/views/admin/customer/create.blade.php
@@ -27,25 +27,22 @@
                         <small id="name-error" class="text-danger" style="display: none;">Vui lòng nhập tên khách hàng!</small>
                     </div>
                     <div class="form-group col-md-4">
+                        <label for="date_of_birth" class="form-label">Ngày sinh<span class="required">*</span></label>
+                        <input type="date" id="date_of_birth" name="date_of_birth" class="form-control" value="{{ old('date_of_birth') }}" required>
+                        <small id="date-error" class="text-danger" style="display: none;">Bạn phải đủ 18 tuổi!</small>
+                        <small id="date-incomplete-error" class="text-danger" style="display: none;">Vui lòng nhập đầy đủ ngày, tháng và năm!</small>
+                    </div>
+                    <div class="form-group col-md-4">
                         <label for="gender" class="form-label">Giới tính<span class="required">*</span></label>
                         <select id="gender" name="gender" class="form-control" required>
                             <option value="Nam" {{ old('gender') == 'Nam' ? 'selected' : '' }}>Nam</option>
                             <option value="Nữ" {{ old('gender') == 'Nữ' ? 'selected' : '' }}>Nữ</option>
                         </select>
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="tax_id" class="form-label">Mã số thuế<span class="required">*</span></label>
-                        <input type="text" id="tax_id" name="tax_id" class="form-control" required pattern="\d{1,9}" title="Mã số thuế chỉ được phép tối đa 9 chữ số" value="{{ old('tax_id') }}">
-                        <small id="tax-error" class="text-danger" style="display: none;">Vui lòng nhập mã số thuế!</small>
-                    </div>
+                    
                 </div>
                 <div class="row mb-3">
-                    <div class="form-group col-md-4">
-                        <label for="date_of_birth" class="form-label">Ngày sinh<span class="required">*</span></label>
-                        <input type="date" id="date_of_birth" name="date_of_birth" class="form-control" value="{{ old('date_of_birth') }}" required>
-                        <small id="date-error" class="text-danger" style="display: none;">Bạn phải đủ 18 tuổi!</small>
-                        <small id="date-incomplete-error" class="text-danger" style="display: none;">Vui lòng nhập đầy đủ ngày, tháng và năm!</small>
-                    </div>
+                    
                     <div class="form-group col-md-6">
                         <label for="phone" class="form-label">Số điện thoại<span class="required">*</span></label>
                         <input type="text" id="phone" name="phone" class="form-control" required pattern="\d{10}" title="Số điện thoại phải gồm 10 chữ số" value="{{ old('phone') }}">
@@ -58,6 +55,11 @@
                         <small id="email-error" class="text-danger">{{ $message }}</small>
                         @enderror
                     </div>
+                    <div class="form-group col-md-4">
+                        <label for="address" class="form-label">Địa chỉ<span class="required">*</span></label>
+                        <input type="text" id="address" name="address" class="form-control" required value="{{ old('address') }}">
+                        <small id="address-error" class="text-danger" style="display: none;">Vui lòng nhập địa chỉ!</small>
+                    </div>
                 </div>
                 <div class="row mb-3">
                     <div class="form-group col-md-6">
@@ -66,22 +68,25 @@
                         <small id="website-error" class="text-danger" style="display: none;">Vui lòng nhập website!</small>
                     </div>
                     <div class="form-group col-md-6">
+                        <label for="software" class="form-label">Phần mềm<span class="required">*</span></label>
+                        <input type="text" id="software" name="software" class="form-control" required value="{{ old('software') }}">
+                        <small id="software-error" class="text-danger" style="display: none;">Vui lòng nhập phần mềm!</small>
+                    </div>
+
+                    
+                </div>
+                <div class="row mb-3">
+                    <div class="form-group col-md-6">
                         <label for="company" class="form-label">Công ty<span class="required">*</span></label>
                         <input type="text" id="company" name="company" class="form-control" required value="{{ old('company') }}">
                         <small id="company-error" class="text-danger" style="display: none;">Vui lòng nhập công ty!</small>
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="software" class="form-label">Phần mềm<span class="required">*</span></label>
-                        <input type="text" id="software" name="software" class="form-control" required value="{{ old('software') }}">
-                        <small id="software-error" class="text-danger" style="display: none;">Vui lòng nhập phần mềm!</small>
+                        <label for="tax_id" class="form-label">Mã số thuế<span class="required">*</span></label>
+                        <input type="text" id="tax_id" name="tax_id" class="form-control" required pattern="\d{1,9}" title="Mã số thuế chỉ được phép tối đa 9 chữ số" value="{{ old('tax_id') }}">
+                        <small id="tax-error" class="text-danger" style="display: none;">Vui lòng nhập mã số thuế!</small>
                     </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="form-group col-md-6">
-                        <label for="address" class="form-label">Địa chỉ<span class="required">*</span></label>
-                        <input type="text" id="address" name="address" class="form-control" required value="{{ old('address') }}">
-                        <small id="address-error" class="text-danger" style="display: none;">Vui lòng nhập địa chỉ!</small>
-                    </div>
+                    
                 </div>
             </div>
             <div class="col-md-3 grouped-fields">
