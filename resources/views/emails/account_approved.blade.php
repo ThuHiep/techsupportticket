3bc093c025e1bcf212cb354df40b368a9759f972
caonguyenquoclam <99850767+QuocLammm@users.noreply.github.com>
2025-01-08 15:54:11 +0700

    update

--- resources/views/emails/account_approved.blade.php
+++ resources/views/emails/account_approved.blade.php
@@ -9,6 +9,6 @@
 <h1>Chúc mừng {{ $customerName }}</h1>
 <p>Tài khoản của bạn đã được phê duyệt thành công!</p>
 <p>Vui lòng đăng nhập và thay đổi mật khẩu trong vòng 24h tới.</p>
-<p><a href="{{ url('/') }}">Website</a></p>
+<p><a href="{{ url('https://techsupportticket.vpsttt.vn/') }}">Website</a></p>
 </body>
 </html>
