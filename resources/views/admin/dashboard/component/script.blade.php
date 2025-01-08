0ac56652d68c62f579b1ba27fa300afba16fe56d
Vinh_Nguyen <128900315+EkkoNguyen@users.noreply.github.com>
2025-01-08 16:04:28 +0700

    fix asset

--- resources/views/admin/dashboard/component/script.blade.php
+++ resources/views/admin/dashboard/component/script.blade.php
@@ -1,47 +1,41 @@
+<!-- Mainly scripts -->
+<script src="{{ asset('admin/js/jquery-3.1.1.min.js') }}"></script>
+<script src="{{ asset('admin/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/chartJs/Chart.min.js') }}"></script>
+<script src="{{ asset('admin/js/demo/chartjs-demo.js') }}"></script>
 
-    <!-- Mainly scripts -->
-    <script src="admin/js/jquery-3.1.1.min.js"></script>
-    <script src="admin/js/bootstrap.min.js"></script>
-    <script src="admin/js/plugins/metisMenu/jquery.metisMenu.js"></script>
-    <script src="admin/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-    <script src="admin/js/plugins/chartJs/Chart.min.js"></script>
-    <script src="admin/js/demo/chartjs-demo.js"></script>
-    
-   <script src="admin/js/plugins/flot/jquery.flot.js"></script>
-   <script src="admin/js/plugins/flot/jquery.flot.pie.js"></script>
-   <script src="admin/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
-   <script src="admin/js/plugins/flot/jquery.flot.spline.js"></script>
-   <script src="admin/js/plugins/flot/jquery.flot.resize.js"></script>
-   <script src="admin/js/demo/peity-demo.js"></script>
-   <script src="admin/js/plugins/flot/jquery.flot.symbol.js"></script>
-   <script src="admin/js/plugins/flot/jquery.flot.time.js"></script>
-   <script src="admin/js/plugins/peity/jquery.peity.min.js"></script>
-   <script src="admin/js/inspinia.js"></script>
-   <script src="admin/js/plugins/iCheck/icheck.min.js"></script>
-   <script src="admin/js/plugins/pace/pace.min.js"></script>
-   <script src="admin/js/plugins/summernote/summernote.min.js"></script>
+<script src="{{ asset('admin/js/plugins/flot/jquery.flot.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/flot/jquery.flot.pie.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/flot/jquery.flot.tooltip.min.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/flot/jquery.flot.spline.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/flot/jquery.flot.resize.js') }}"></script>
+<script src="{{ asset('admin/js/demo/peity-demo.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/flot/jquery.flot.symbol.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/flot/jquery.flot.time.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/peity/jquery.peity.min.js') }}"></script>
+<script src="{{ asset('admin/js/inspinia.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/iCheck/icheck.min.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/pace/pace.min.js') }}"></script>
+<script src="{{ asset('admin/js/plugins/summernote/summernote.min.js') }}"></script>
 
 <!-- SUMMERNOTE -->
 
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
 
         $('.summernote').summernote();
 
     });
-
 </script>
 
 
-    <!--Đoạn gốc ban đầu-->
-     @if(isset($config['js']) && is_array($config['js']))
-        @foreach($config['js'] as $key => $val)
+<!--Đoạn gốc ban đầu-->
+@if(isset($config['js']) && is_array($config['js']))
+@foreach($config['js'] as $key => $val)
 
-        {!! '<script src = "'.$val.'"></script>'!!}
+{!! '<script src="'.$val.'"></script>'!!}
 
-        @endforeach
-     @endif
-     
-
-
-     
+@endforeach
+@endif
--- resources/views/admin/request/reply-cus.blade.php
+++ resources/views/admin/request/reply-cus.blade.php
@@ -2,6 +2,7 @@
 <html>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
 
+<script src="{{ asset('admin/js/jquery-3.1.1.min.js') }}"></script>
 
 <body>
     <div class="animated fadeInRight">
